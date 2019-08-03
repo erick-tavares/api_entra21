5d3ca99e5bcabfe385492a7821d9d80f395f1fa9
“erick-tavares” <erick-tavares@hotmail.com>
2019-08-02 21:00:31 -0300

    criado class My Application

---
+++ src/main/java/br/com/proway/api/MyApplication.java
@@ -0,0 +1,13 @@
+package br.com.proway.api;
+
+import javax.ws.rs.ApplicationPath;
+
+import org.glassfish.jersey.server.ResourceConfig;
+
+@ApplicationPath("rest")
+public class MyApplication extends ResourceConfig{
+
+	public MyApplication() {
+		packages ("br.com.proway.api;controller");
+	}
+}
